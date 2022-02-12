ad097d38db71bd009619739feb168c59378dd0ca
Serge <srg404@gmail.com>
2022-02-12 18:14:32 +0100

    improve asimov animation

--- src/js/modules/animations.js
+++ src/js/modules/animations.js
@@ -4,8 +4,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const animations = () => {
-  console.log("hello World of Animations !");
-
   const asimov2 = gsap
     .timeline({
       scrollTrigger: {
@@ -18,18 +16,18 @@ const animations = () => {
     })
     .fromTo(
       "#section-asimov-2 .element-2",
-      { y: 0, x: "-110%" },
-      { y: 0, x: 0 },
+      { y: -1, x: "-110%" },
+      { y: -1, x: 0 },
       0
     )
-    .set("#section-asimov-2 .element-1", { y: "-82.5%" })
+    .set("#section-asimov-2 .element-1", { y: "-79%" })
     .fromTo(
       "#section-asimov-2 .element-2",
       { y: "100%", x: "110%" },
       { y: "100%", x: 0 },
       1
     )
-    .set("#section-asimov-2 .element-1", { y: "-62.5%" })
+    .set("#section-asimov-2 .element-1", { y: "-59%" })
     .fromTo(
       "#section-asimov-2 .element-2",
       { y: "200%", x: "-110%" },
@@ -38,26 +36,36 @@ const animations = () => {
     )
     .fromTo(
       "#section-asimov-2 .quote",
-      { autoAlpha: 0, x: -100 },
+      { autoAlpha: 0, x: "-150%" },
       { autoAlpha: 1, x: 0 },
       2
     )
-    .set("#section-asimov-2 .element-1", { y: "-42.5%" })
+    .set("#section-asimov-2 .element-1", { y: "-39%" })
     .fromTo(
       "#section-asimov-2 .element-2",
       { y: "300%", x: "110%" },
       { y: "300%", x: 0 },
       3
     )
-    .set("#section-asimov-2 .element-1", { y: "-22.5%" })
+    .set("#section-asimov-2 .element-1", { y: "-19%" })
     .fromTo(
       "#section-asimov-2 .element-2",
       { y: "400%", x: "-110%" },
       { y: "400%", x: 0 },
       4
     )
-    .set("#section-asimov-2 .element-1", { y: "0" })
+    .set("#section-asimov-2 .element-1", { y: "1%" })
     .set("#section-asimov-2 .element-2", { y: "0", x: "-110%" });
+
+  const arendt = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#section-arendt-1",
+      pin: true,
+      start: "top top",
+      end: "center top",
+      scrub: true,
+    },
+  });
 };
 
 export default animations;
